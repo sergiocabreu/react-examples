ec0e26fad409e6b7c40bd1a72c74107a1e229fe4
Sérgio C. Abreu <sergiocabreu@gmail.com>
2021-05-27 17:08:11 -0300

    Implementação de um formulário básico.

--- formularios/src/App.js
+++ formularios/src/App.js
@@ -1,10 +1,29 @@
-import React from 'react'
-import './App.css';
+import { Container, Typography } from "@material-ui/core";
+import React from "react";
+import "./App.css";
+import FormularioCadastro from "./components/formularioCadastro/FormularioCadastro";
 
 function App() {
   return (
-    <h1>Formulário de cadastro</h1>
+    <Container component="article" maxWidth="sm">
+      <Typography variant="h3" component="h1" align="center">
+        Formulário de cadastro
+      </Typography>
+      <FormularioCadastro aoEnviar={aoEnviarForm} validarCpf={validarCpf} />
+    </Container>
   );
+}
+
+function aoEnviarForm(dados) {
+  console.log(dados);
+}
+
+function validarCpf(cpf) {
+  if (cpf.length !== 11) {
+    return { valido: false, texto: "CPF deve ter 11 digitos" };
+  } else {
+    return { valido: true, texto: "" };
+  }
 }
 
 export default App;
