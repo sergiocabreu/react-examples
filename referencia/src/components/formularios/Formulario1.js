80feb185b31985d8a61383a7890d61693c818e58
Sérgio C. Abreu <sergiocabreu@gmail.com>
2021-07-22 14:34:00 -0300

    Inclusão de exemplo de formulário.
    Remorção de arquivos desnecessários.

---
+++ referencia/src/components/formularios/Formulario1.js
@@ -0,0 +1,56 @@
+import React, {Component} from 'react';
+
+
+class Formulario1 extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            email : "",
+            senha: "",
+            sexo: "masculino"
+        }
+
+        this.trocaEmail = this.trocaEmail.bind(this);
+        this.trocaSenha = this.trocaSenha.bind(this);
+    }
+
+    trocaEmail(event) {
+        const valorDigitado = event.target.value;
+        this.setState({email: valorDigitado});
+    }
+
+    trocaSenha(event) {
+        const valorDigitado = event.target.value;
+        this.setState({senha: valorDigitado});
+    }
+
+    render() {
+        return (
+            <div>
+                <h3>Login</h3>
+
+                <label>Email</label>
+                <input type="text" name="email" value={this.state.email} onChange={this.trocaEmail}/><br/>
+
+                <label>Senha</label>
+                <input type="password" name="senha" value={this.state.senha} onChange={this.trocaSenha}/><br/>
+
+                <select 
+                    name="sexo" 
+                    value={this.state.sexo} 
+                    onChange={ (e) => { this.setState({sexo: e.target.value}) } }>
+                    <option value="masculino">Masculino</option>
+                    <option value="femino">Feminino</option>
+                </select>
+
+                <h3>{this.state.email}</h3>
+                <h3>{this.state.senha}</h3>
+                <h3>{this.state.sexo}</h3>
+            </div>
+        );
+    }
+}
+
+
+export default Formulario1;
