ae851810e498f21dd4e91349b21feeac2c480651
Sérgio C. Abreu <sergiocabreu@gmail.com>
2021-08-06 08:55:02 -0300

    Inclusão de projeto firebase.

---
+++ firebase-ref/src/App.js
@@ -0,0 +1,11 @@
+import firebase from './firebaseConnection'
+
+function App() {
+  return (
+    <div>
+      <h1>React Js + Firebase</h1>
+      </div>
+  );
+}
+
+export default App;
---
+++ firebase-ref/src/firebaseConnection.js
@@ -0,0 +1,17 @@
+import firebase from 'firebase/app';
+import 'firebase/firestore';
+
+var firebaseConfig = {
+    apiKey: "AIzaSyDSBSnuCPway7FbXwpMi95hpiwkRCCDP6E",
+    authDomain: "desenvolvimento-b28ea.firebaseapp.com",
+    projectId: "desenvolvimento-b28ea",
+    storageBucket: "desenvolvimento-b28ea.appspot.com",
+    messagingSenderId: "34466355116",
+    appId: "1:34466355116:web:491bcd4c29972446b94c09"
+  };
+
+  if(!firebase.apps.length) {
+      firebase.initializeApp(firebaseConfig);
+  }
+
+  export default firebase;
